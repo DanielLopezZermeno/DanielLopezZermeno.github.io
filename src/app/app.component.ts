dde7796466751234bd78febf4702416a213102ab
DanielLopezZermeno <86745921+DanielLopezZermeno@users.noreply.github.com>
2023-06-08 14:28:45 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, VERSION } from '@angular/core';
+import { CarteleraService } from './cartelera/cartelera.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
